eb86badfe4b0d4a676ab08bdc0bb44e0e479d0b9
Denis Neverov <denis.neverov@gmail.com>
2020-05-31 02:09:40 +0300

    Create lib Helloworld module

--- lib/helloworld.rb
+++ lib/helloworld.rb
@@ -1,6 +1,10 @@
+require "helloworld/ffi"
 require "helloworld/version"
 
 module Helloworld
   class Error < StandardError; end
-  # Your code goes here...
+
+  def self.say_hello
+    FFI.hello_world
+  end
 end
